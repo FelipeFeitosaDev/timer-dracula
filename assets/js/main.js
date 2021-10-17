c8ce0663aa828d5e0602c08af930cc2b8ac307b1
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-10-17 07:52:45 -0300

    button elements has been queried

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 function escope(){
+    const startButton = document.querySelector('.start');
+    const pauseButton = document.querySelector('.pause');
+    const resteButton = document.querySelector('.reset');
 
 }
 
