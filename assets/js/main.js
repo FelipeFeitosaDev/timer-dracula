01505fb244e879f45b4e09348d8b54261d2a5b2a
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-10-17 08:10:51 -0300

    start timer has been created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 function escope(){
+    const timerElement = document.querySelector('.timer');
     const startButton = document.querySelector('.start');
     const pauseButton = document.querySelector('.pause');
     const resteButton = document.querySelector('.reset');
 
     let miliseconds = 0;
+    let start;
 
     function getHour(miliseconds){
         const date = new Date(miliseconds);
@@ -17,6 +19,19 @@ function escope(){
 
     }
 
+    document.addEventListener('click', (e)=>{
+        
+        if(e.target.classList.contains('start')){
+            start = setInterval(()=>{
+                miliseconds += 1000;
+                const timer = getHour(miliseconds);
+                timerElement.innerHTML = `${timer}`
+    
+            }, 1000);
+        }
+        
+      
+    })
 }
 
 escope();
